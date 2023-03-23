922715ffb70d87a6d58f4336c06d9756f3c2c9b0
TomaSilva1 <tomasilva302@gmail.com>
2023-03-23 18:39:08 -0300

    5to commit

---
+++ tp2_1_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define N 20
+
+int main(){
+
+    int i;
+    double vt[N], *punt;
+
+    for(i = 0;i<N; i++)
+    {
+        vt[i]=1+rand()%100;
+        punt = &vt[i];
+        printf("%.2f ", *punt);
+        punt++;
+    }
+}
