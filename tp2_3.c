c72124baabb8e5a8556ba4c3239d41710b1b6599
TomaSilva1 <tomasilva302@gmail.com>
2023-03-23 19:06:05 -0300

    7mo commit

---
+++ tp2_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define N 5
+#define M 7
+
+int main(){
+
+    int i,j;
+    int mt[N][M];
+    int *punt=&mt[0][0];
+
+    for(i = 0;i<N; i++)
+    {
+        for(j = 0;j<M; j++)
+        {
+            *punt=1+rand()%100;
+            printf("%d ", *punt);
+            punt++;
+        }
+            printf("\n");
+    }
+}
